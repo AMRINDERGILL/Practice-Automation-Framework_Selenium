3e3eb4dcf8f9eafd17047642fa31ba0cb67af98f
Amrinder <amrindergill@gmail.com>
2025-08-25 15:50:48 +0530

    Chrome test added

---
+++ src/test/java/testcase/Chrome.java
@@ -0,0 +1,15 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class Chrome extends BaseTest {
+
+	@Test
+	public void chromeLaunch()
+	{
+		driver.get("www.google.com");
+	}
+}
+
